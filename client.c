e5aa78c5546bef6a89c5eb629ebb6070d7bf66c0
QuentinBonvin <qbonvin@student.42lausanne.ch>
2022-02-15 16:24:10 +0100

    second commit

--- client.c
+++ client.c
@@ -6,19 +6,114 @@
 /*   By: qbonvin <qbonvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 14:03:20 by qbonvin           #+#    #+#             */
-/*   Updated: 2022/02/09 11:36:24 by qbonvin          ###   ########.fr       */
+/*   Updated: 2022/02/15 16:22:54 by qbonvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
 
+int kill( pid_t pid, int sig );
+int	ft_atoi(const char *str);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int pid_t = getpid();
-    int pid_d = getppid();
-    printf("pid = %d", pid_t);
-    printf("pid = %d", pid_d);
+    int 	pid = ft_atoi(argv[1]);
+    int 	second = ft_atoi(argv[2]);
+    if (argc != 3)
+    {
+        printf("There isn't 2 arguments (PID | STRING");
+        return (0);
+    }
+    kill(pid, SIGUSR1);
+    kill(second, SIGUSR2);
     return (0);
 }
+
+
+int	ft_atoi(const char *str)
+{
+	int	index;
+	int	sign;
+	int	res;
+
+	res = 0;
+	sign = 1;
+	index = 0;
+	while ((str[index] >= 9 && str[index] <= 13) || str[index] == 32)
+	{
+		index++;
+	}
+	if (str[index] == '-' || str[index] == '+')
+	{
+		if (str[index] == '-')
+			sign *= -1;
+		index++;
+	}
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		res = res * 10 + str[index] - '0';
+		index++;
+	}
+	return (res * sign);
+}
+
+/**********************************************************************************/
+
+static int	len_number(long *n)
+{
+	int		len;
+	long	n2;
+
+	len = 0;
+	if (*n == 0)
+	{
+		len = 1;
+	}
+	if (*n < 0)
+	{
+		*n = *n * -1;
+		len++;
+	}
+	n2 = *n;
+	while (n2 > 0)
+	{
+		n2 = n2 / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*string;
+	int		len_str;
+	long	nbr;
+
+	nbr = n;
+	len_str = len_number(&nbr);
+	string = malloc(sizeof(char) * (len_str + 1));
+	if (!string)
+		return (0);
+	string[len_str] = '\0';
+	len_str--;
+	if (nbr == 0)
+	{
+		string[0] = 48;
+		return (string);
+	}
+	if (n < 0)
+		string[0] = '-';
+	while (nbr > 0)
+	{
+		string[len_str] = 48 + (nbr % 10);
+		nbr = nbr / 10;
+		len_str--;
+	}
+	return (string);
+}
+
+/********************************************************************************/
--- server.c
+++ server.c
@@ -6,18 +6,39 @@
 /*   By: qbonvin <qbonvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 14:03:59 by qbonvin           #+#    #+#             */
-/*   Updated: 2022/02/09 10:14:30 by qbonvin          ###   ########.fr       */
+/*   Updated: 2022/02/15 16:22:54 by qbonvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void    signal_received(int sig);
+
+void    signal_received(int sig)
+{
+    if (sig == SIGUSR1)
+    {
+        write(1, "j'ai recu le signal", 19);
+    }
+    if (sig == SIGUSR2)
+        write(1, "j'ai recu le 2eme signal", 24);
+    return ;
+}
 
 int main(void)
 {
     int pid_t = getpid();
-    int pid_p = getppid();
-    printf("pid = %d", pid_t);
-    printf("pid = %d", pid_p);
+    printf("%d\n", pid_t);
+    while (1)
+    {
+        signal(SIGUSR1, signal_received);
+        pause();
+        return (0);
+    }
     return (0);
 }
+
